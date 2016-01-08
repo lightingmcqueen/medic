6c593b632a08555536279292037c49bff41d03b7
Umapathy <umapathymanohar@gmail.com>
2016-01-08 15:21:45 +0530

    schema changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :staffs
   resources :clinics
   resources :subcategories
+  mount RailsAdmin::Engine => '/administration', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
